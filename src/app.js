ab1ce9baace3972dcec913b2104b4d1a14b19657
fergabrielp <perezfernando95@gmail.com>
2023-06-15 03:57:27 -0300

    Testing server conecction

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Holaass")
+const express = require('express')
+
+const PORT = 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Testing server...")
+})
+
+app.listen(PORT, () => {
+    console.log(`Listenting to request in port ${PORT}`)
+})
